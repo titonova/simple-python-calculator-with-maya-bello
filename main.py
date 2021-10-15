b4c8511388c5dcfcbc03f20e53b0bd0b9a710d9e
titonova <41286822+titonova@users.noreply.github.com>
2021-10-15 10:50:24 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+print("Savings Calculator")
+user_input = input("How much do you make per hour? ")
+hours_per_day = 8
+
+daily_savings = int(user_input) * hours_per_day
+print("daily savings:", daily_savings)
+
+weekly_savings = daily savings * 5 
+print("weekly savings:", weekly_savings)
+
+monthly_savings = weekly_savings * 4
+print("monthly savings:", monthly_savings)
